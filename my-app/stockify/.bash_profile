f3445e4de45806a46acc1846324bf297ca6f822c
VIVEK RAMJI <vivekr@VIVEKs-MacBook-Air.local>
2017-10-10 14:10:32 -0400

    deleting older version

--- my-app/stockify/.bash_profile
+++
@@ -1,2 +0,0 @@
-export M2_HOME=/Users/vivekr/Applications/apache-maven-3.5.0
-export PATH=$PATH:$M2_HOME/bin
--- my-app/stockify/src/main/java/com/stockify/stockify/service/StocksService.java
+++
@@ -1,9 +0,0 @@
-package com.stockify.stockify.service;
-
-import java.util.Collection;
-
-import com.stockify.stockify.model.Stock;
-
-public interface StocksService {
-	public abstract Collection<Stock> getAllStocks();
-}
